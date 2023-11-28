40be2e0b02fb9ce8fd262732ab8a811aba186dfc
Santiago Ramos <117044142+santiagoramosg@users.noreply.github.com>
2023-11-28 18:36:51 -0500

    Add files via upload

---
+++ CreateFriend/src/main/java/com/mycompany/createfriend/CreateFriend.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.createfriend;
+
+
+
+public class CreateFriend {
+
+    public static void main(String[] args) {      
+        interfaz form=new interfaz();
+        form.setVisible(true);
+    }
+}
